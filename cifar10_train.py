afdd7595ee844171c2b82d2b5b7bbe68fde9cdb3
lorne <1991wangliang@gmail.com>
2023-05-31 12:46:03 +0800

    add mian

--- cifar10_train.py
+++ cifar10_train.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 
 import torch
 import torch.nn as nn
@@ -11,6 +12,11 @@ from cifar10_model import Net
 os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7890'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7890'
 
+batch_size = 4
+epochs = 100
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -18,12 +24,12 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
@@ -31,60 +37,63 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 
 # 实例化网络
-net = Net()
+net = Net().to(device)
 
 
 # 定义损失函数和优化器
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-epochs = 100
+def train():
+    # 训练网络
+    for epoch in range(epochs):  # loop over the dataset multiple times
 
-# 训练网络
-for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    torch.save(net.state_dict(), './models/model.pth')
+    print('Finished Training')
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:
-            # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
 
-print('Finished Training')
+def predict():
+    net.eval()  # 设置模型为评估模式
 
-torch.save(net.state_dict(), './models/model.pth')
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-model = Net()  # 创建一个新的模型实例
-model.load_state_dict(torch.load('./models/model.pth'))  # 加载模型参数
-model.eval()  # 设置模型为评估模式
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
 
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = model(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    train()
+    predict()

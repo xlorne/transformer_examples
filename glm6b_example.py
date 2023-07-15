749089af3005a5ce4deb413d9559060a0f4b4eeb
root <1991wangliang@gmail.com>
2023-07-15 01:01:33 +0000

    fix glm6b

--- glm6b_example.py
+++ glm6b_example.py
@@ -10,14 +10,16 @@ os.environ['HTTP_PROXY'] = proxy
 os.environ['HTTPS_PROXY'] = proxy
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("/code/lorne/models/chatglm-6b", trust_remote_code=True)
 
-# print(tokenizer)
+print(tokenizer)
 
-model = AutoModelForCausalLM.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).half().cuda()
+model = AutoModel.from_pretrained("/code/lorne/models/chatglm-6b", trust_remote_code=True).half().cuda()
 model = model.eval()
+
+print(model)
 
 response, history = model.chat(tokenizer, "你好", history=[])
 

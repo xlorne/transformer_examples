050cabf96a16576ec16ec4dfcd8bc3535778c258
root <1991wangliang@gmail.com>
2023-07-17 06:49:46 +0000

    fix 7b

--- llm/internlm7b_example.py
+++ llm/internlm7b_example.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("/code/lorne/models/internlm-chat-7b")
+tokenizer = AutoTokenizer.from_pretrained("/code/lorne/models/internlm-chat-7b",trust_remote_code=True)
 
-model = AutoModelForCausalLM.from_pretrained("/code/lorne/models/internlm-chat-7b")
+model = AutoModelForCausalLM.from_pretrained("/code/lorne/models/internlm-chat-7b",trust_remote_code=True).cuda()
 model = model.eval()
 
 response, history = model.chat(tokenizer, "你好", history=[])

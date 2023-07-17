e44e76357058c48749bfe9a0a28eadcc8d3fdea4
lorne <1991wangliang@gmail.com>
2023-07-17 14:42:26 +0800

    add internlm

--- llm/internlm7b_example.py
+++ llm/internlm7b_example.py
@@ -2,22 +2,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("/code/lorne/models/internlm-chat-7b")
 
-# print(tokenizer)
-
 model = AutoModelForCausalLM.from_pretrained("/code/lorne/models/internlm-chat-7b")
+model = model.eval()
 
-input_context = "你好"
-input_ids = tokenizer.encode(input_context, return_tensors='pt')
-#
-# dicts = tokenizer.get_vocab()
-# print(dicts)
-# print(input_ids)
-
-print(model)
-
-# Generate 50 tokens
-output = model.generate(input_ids, max_length=50)
-
-output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-print(output_text)
-
+response, history = model.chat(tokenizer, "你好", history=[])
+print(response)

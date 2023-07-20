ddbb7e976c47ff8a043ad6b64b89b2f44ed0de80
lorne <1991wangliang@gmail.com>
2023-07-20 08:38:32 +0800

    add 350m

--- llm/opt350b_example.py
+++ llm/opt350b_example.py
@@ -9,12 +9,14 @@ model = model.eval()
 
 parser = argparse.ArgumentParser(description="Predict Argparse")
 parser.add_argument("--question", "-q", help="问题内容")
+parser.add_argument("--length", "-l", help="输出问题长度", default=100)
 args = parser.parse_args()
 
 if __name__ == '__main__':
     input_context = args.question
+    max_length = args.length
     input_ids = tokenizer.encode(input_context, return_tensors='pt')
     input_ids = input_ids.cuda()
-    output = model.generate(input_ids, max_length=50)
+    output = model.generate(input_ids, max_length=max_length)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print(output_text)

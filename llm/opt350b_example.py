ca6b1e19671471eda2a5383b11f04880a4fa5846
lorne <1991wangliang@gmail.com>
2023-07-20 08:36:07 +0800

    add 350m

--- llm/opt350b_example.py
+++ llm/opt350b_example.py
@@ -1,24 +1,20 @@
+import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
+tokenizer = AutoTokenizer.from_pretrained("/mnt/lorne/transformer_examples/llm/trained_model")
 
-print(tokenizer)
-
-model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m").cuda()
+model = AutoModelForCausalLM.from_pretrained("/mnt/lorne/transformer_examples/llm/trained_model").cuda()
 model = model.eval()
 
-input_context = "孔明什么时候与刘备结识的？"
-input_ids = tokenizer.encode(input_context, return_tensors='pt')
-input_ids = input_ids.cuda()
-#
-# dicts = tokenizer.get_vocab()
-# print(dicts)
-# print(input_ids)
 
-print(model)
+parser = argparse.ArgumentParser(description="Predict Argparse")
+parser.add_argument("--question", "-q", help="问题内容")
+args = parser.parse_args()
 
-# Generate 50 tokens
-output = model.generate(input_ids, max_length=50)
-
-output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-print(output_text)
+if __name__ == '__main__':
+    input_context = args.question
+    input_ids = tokenizer.encode(input_context, return_tensors='pt')
+    input_ids = input_ids.cuda()
+    output = model.generate(input_ids, max_length=50)
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(output_text)
